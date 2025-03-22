b0a066136c183197e497a5809195a9505cdfd953
Dark <131844488+DarkPlayOff@users.noreply.github.com>
2025-03-22 11:00:37 +0300

    Update VanillaGoal.java

--- paper-api/src/generated/java/com/destroystokyo/paper/entity/ai/VanillaGoal.java
+++ paper-api/src/generated/java/com/destroystokyo/paper/entity/ai/VanillaGoal.java
@@ -1,18 +1,1 @@
-r.class);
 
-    GoalKey<WanderingTrader> WANDER_TO_POSITION = create("wander_to_position", WanderingTrader.class);
-
-    GoalKey<Wither> WITHER_DO_NOTHING = create("wither_do_nothing", Wither.class);
-
-    GoalKey<Wolf> BEG = create("beg", Wolf.class);
-
-    GoalKey<Wolf> WOLF_AVOID_ENTITY = create("wolf_avoid_entity", Wolf.class);
-
-    GoalKey<Zombie> ZOMBIE_ATTACK = create("zombie_attack", Zombie.class);
-
-    GoalKey<Zombie> ZOMBIE_ATTACK_TURTLE_EGG = create("zombie_attack_turtle_egg", Zombie.class);
-
-    private static <T extends Mob> GoalKey<T> create(final String key, final Class<T> type) {
-        return GoalKey.of(type, NamespacedKey.minecraft(key));
-    }
-}
