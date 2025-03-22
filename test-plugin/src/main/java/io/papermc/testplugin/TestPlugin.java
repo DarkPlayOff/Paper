4db1c5ed7aca1a208b2def57145aa7a9eda8d931
Dark <131844488+DarkPlayOff@users.noreply.github.com>
2025-03-22 10:57:16 +0300

    Update TestPlugin.java

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -1,14 +1,1 @@
-package io.papermc.testplugin;
 
-import org.bukkit.event.Listener;
-import org.bukkit.plugin.java.JavaPlugin;
-
-public final class TestPlugin extends JavaPlugin implements Listener {
-
-    @Override
-    public void onEnable() {
-        this.getServer().getPluginManager().registerEvents(this, this);
-
-        // io.papermc.testplugin.brigtests.Registration.registerViaOnEnable(this);
-    }
-}
